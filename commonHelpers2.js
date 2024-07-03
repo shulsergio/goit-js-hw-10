634fdd9c1ead17f5ef58edf92dda00c6d5b0b4fe
shulsergio <101953752+shulsergio@users.noreply.github.com>
2024-07-03 20:47:53 +0000

    Deploying to gh-pages from @ shulsergio/goit-js-hw-10@1a6835278fce21488fd84f5ac041ef6ec88dd1fd 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";o.info({backgroundColor:"#09f",title:"Hello",titleColor:"#fff",titleSize:"16px",message:"Welcome!",messageColor:"#fff",messageSize:"16px",theme:"light",position:"topRight",iconColor:"#fff"});
+//# sourceMappingURL=commonHelpers2.js.map
