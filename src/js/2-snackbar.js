949109a3f885dc1f003346c4591c41753f3c1dcd
Sergey Shulga <shul.sergio@gmail.com>
2024-07-04 16:31:13 +0300

    v1.6 finish snackbar
    v1.6 finish snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -15,3 +15,43 @@ iziToast.info({
   position: 'topRight',
   iconColor: '#fff',
 });
+const dataInput = document.querySelector('input[type="number"]');
+const btnSubmit = document.querySelector('.btn-sbm');
+btnSubmit.addEventListener('click', onClickBtn);
+
+function onClickBtn(evt) {
+  evt.preventDefault();
+  let radio = document.querySelector('input[name="state"]:checked').value;
+  let delay = parseInt(dataInput.value);
+
+  const mypromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      radio === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+  mypromise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${delay}ms`,
+        messageSize: '16px',
+        color: '#59a10d',
+        messageColor: '#fff',
+        titleColor: '#fff',
+        iconColor: '#fff',
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${delay}ms`,
+        messageSize: '16px',
+        color: '#ef4040',
+        messageColor: '#fff',
+        titleColor: '#fff',
+        iconColor: '#fff',
+        position: 'topRight',
+      });
+    });
+}
